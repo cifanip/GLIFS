4151cfde22a1e1fbcd88178a75499f1ab6576954
Paolo Cifani <cifani.pl@gmail.com>
2022-03-04 12:02:20 +0100

    Add files via upload

---
+++ main.F90
@@ -0,0 +1,36 @@
+PROGRAM main
+
+  USE solver_mod
+
+  IMPLICIT NONE
+  
+  type(solver) :: qturb
+  real(double_p) :: ts,te
+  type(par_file) :: pfile
+  integer :: flow
+  
+  call init_run()
+  
+  ts = MPI_Wtime()
+  
+  call pfile%ctor('input_parameters','specs')
+  call pfile%read_parameter(flow,'flow')
+  
+  call qturb%ctor()
+  
+  select case(flow)
+    case(EULER)
+      call qturb%solve_euler()
+    case(H_TURB)
+      call qturb%solve_hturb()
+    case default
+      call abort_run('Wrong flow solver')
+  end select
+  
+  te = MPI_Wtime()
+  
+  call info_run_end(ts,te)
+  
+  call end_run()
+
+END PROGRAM main
