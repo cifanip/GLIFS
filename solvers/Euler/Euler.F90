e809c601d7d475bb6fc441aa2032633f21c48889
Paolo Cifani <cifani.pl@gmail.com>
2022-11-30 09:24:27 +0100

    Update Euler.F90

--- solvers/Euler/Euler.F90
+++ solvers/Euler/Euler.F90
@@ -143,7 +143,7 @@ contains
     !write sph coefficients of vorticity
     call compute_sph_coeff(lap,w,run_time%output_dir,fields_dir)
         
-    !write sph coefficients of vorticity
+    !write sph coefficients of stream function
     call compute_sph_coeff(lap,psi,run_time%output_dir,fields_dir)
     
     !write discrete diffeomorphism (if computed)
