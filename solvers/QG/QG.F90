d558ebf2603968f7c537a7a2a128c89c676672ba
Paolo Cifani <cifani.pl@gmail.com>
2022-11-30 09:25:42 +0100

    Update QG.F90

--- solvers/QG/QG.F90
+++ solvers/QG/QG.F90
@@ -157,7 +157,7 @@ contains
     !write sph coefficients of vorticity
     call compute_sph_coeff(lap,w,run_time%output_dir,fields_dir)
         
-    !write sph coefficients of vorticity
+    !write sph coefficients of stream function
     call compute_sph_coeff(lap,psi,run_time%output_dir,fields_dir)
 
     !write discrete diffeomorphism (if computed)
